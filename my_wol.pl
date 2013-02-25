b3ea35e92d84db01bfac7bec71a542fe6c6c902f
Giulio Mecocci <gm2211@doc.ic.ac.uk>
2013-02-25 19:05:58 +0000

    Completed test_strategies, still printing out debugging messages. Going to clean it in the next commit

--- my_wol.pl
+++ my_wol.pl
@@ -1,17 +1,41 @@
+:- use_module(library(system)).
+% Initialising variable LM to a very high value, so that it
+% will be probable that some value in the "simulation" is
+% smaller than it, therefore setting a new limit. (Ideally
+% +infinty should be chosen).
 test_strategy(0, _, _).
 test_strategy(N, S1, S2) :-
-    test_strategy_helper(N, 0, S1, S2, 0, 0, 0, 0, 0, 0, 0). 
+    test_strategy_helper(N, N, S1, S2, 0, 0, 0, 0, 10000000, 0, 0). 
 
 
-test_strategy_helper(N, 0, S1, S2, W1, W2, D, LM, SM, SUM_MOV, SUM_TIME) :-  
+test_strategy_helper(N, 0, _, _, W1, W2, D, LM, SM, Sum_Mov, Sum_Time) :-  
     format('~nblue wins = ~w~nred wins= ~w~ndraws = ~w~n', [W1,W2,D]),
-    format('~Shortest moves = ~w~nLonges moves = ~w~nAverage moves = ~w~n',
-           [LM, SM, SUM_MOV / N]).
-                                                                             
+    Avg_Mov is Sum_Mov / N,
+    format('~nShortest moves = ~w~nLonges moves = ~w~nAverage moves = ~w~n',
+       [LM, SM, Avg_Mov]),
+    Avg_Time is Sum_Time / N,
+    format('~nAverage play time = ~w', [Avg_Time]),
     format('~nblue score = ~w~nredscore = ~w~n~n', [1,2]).
-test_strategy_helper(N, CNT, S1, S2, W1, W2, D, LM, SM, SUM_MOV, SUM_TIME) :-
-    now(BEFORE),
-    play(quiet, S1, S2, N, _),
-    now(AFTER),
-    NEW_TIME = SUM_TIME + (AFTER - BEFORE),
+test_strategy_helper(N, Cnt, S1, S2, W1, W2, D, LM, SM, Sum_Mov, Sum_Time) :-
+    now(Before),
+    play(quiet, S1, S2, Moves, Winner),
+    now(After),
+    New_Sum_Time is Sum_Time + (After - Before),
+    update_score(Winner, Upd_W1, Upd_W2, Upd_D),
+    format('~nWinner = ~w~n Upd_W1 = ~w~n Upd_W2 = ~w~n Upd_D = ~w~n', [Winner,Upd_W1, Upd_W2, Upd_D]),
+    New_W1 is (Upd_W1 + W1),
+    New_W2 is (Upd_W2 + W2),
+    New_D is (Upd_D + D),
+    ((Moves < LM) -> (New_LM is Moves); (New_LM is LM)),
+    ((Moves > SM) -> (New_SM is Moves);(New_SM is SM)),
+    New_Sum_Mov is (Sum_Mov + Moves),
+    New_Cnt is (Cnt - 1),
+    test_strategy_helper(N,New_Cnt,S1,S2,New_W1,New_W2,New_D,New_LM,New_SM, New_Sum_Mov, New_Sum_Time).
+    
+update_score(Winner, R, B, D) :-
+    ((Winner == r) -> (R is 1); (R is 0)),
+    ((Winner == b) -> (B is 1); (B is 0)),
+    ((Winner == d) -> (D is 1);
+     (Winner == s) -> (D is 1);(D is 0)).
+    
     
